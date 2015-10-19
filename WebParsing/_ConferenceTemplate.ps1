ccad383ab07aa20a60f03b7d46e72b7701d58cb4
Adam Bertram <adbertram@gmail.com>
2015-10-19 16:14:44 -0500

    added conference template

---
+++ WebParsing/_ConferenceTemplate.ps1
@@ -0,0 +1,15 @@
+﻿param (
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[Microsoft.PowerShell.Commands.HtmlWebResponseObject]$WebResponse
+)
+
+###############
+## Insert code to create two datetime objects (Start and End) here
+###############
+
+## Output a hashtable with a Start and an End key
+@{
+	'Start' = [datetime]$startDate
+	'End' = [datetime]$endDate
+} 
