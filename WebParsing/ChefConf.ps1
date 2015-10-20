4fc4cc9e3ee4bd3daebb8f3f9ddfdaf234057fde
Adam Bertram <adbertram@gmail.com>
2015-10-20 09:13:39 -0500

    updated

--- WebParsing/ChefConf.ps1
+++ WebParsing/ChefConf.ps1
@@ -1,10 +1,4 @@
-﻿param (
-	[Parameter(Mandatory)]
-	[ValidateNotNullOrEmpty()]
-	[Microsoft.PowerShell.Commands.HtmlWebResponseObject]$WebResponse
-)
-
-[int]$year = $webResponse.ParsedHtml.body.getElementsByClassName('chefconf-title')[0].innerText.split(' ')[1]
+﻿[int]$year = $webResponse.ParsedHtml.body.getElementsByClassName('chefconf-title')[0].innerText.split(' ')[1]
 $roughdates = $webResponse.ParsedHtml.body.getElementsByClassName('date')[0].innerText.Split('|')[1].Trim()
 $dates = [regex]::Match($roughdates, '(^\w+) (\d+)-(\d+)').Groups.Value
 $month = $dates[1]
--- WebParsing/_ConferenceTemplate.ps1
+++ WebParsing/_ConferenceTemplate.ps1
@@ -1,5 +1,6 @@
 ﻿###############
 ## Insert code to create two datetime objects (Start and End) here
+## Use $WebResponse as the output of Invoke-WebRequest -Uri <ConferenceURL>
 ###############
 
 ## Output a hashtable with a Start and an End key
