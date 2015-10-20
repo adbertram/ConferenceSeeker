f632d45d09300455d9ed3f313b3f1783e61d8a3d
Brandon Olin <brandon@devblackops.io>
2015-10-19 21:36:52 -0700

    Adding ChefConf 2016

---
+++ WebParsing/ChefConf.ps1
@@ -0,0 +1,15 @@
+﻿param (
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[Microsoft.PowerShell.Commands.HtmlWebResponseObject]$WebResponse
+)
+$webResponse = Invoke-WebRequest -Uri $confInfo.Url
+
+[int]$year = $webResponse.ParsedHtml.body.getElementsByClassName('chefconf-title')[0].innerText.split(' ')[1]
+$roughdates = $webResponse.ParsedHtml.body.getElementsByClassName('date')[0].innerText.Split('|')[1].Trim()
+$dates = [regex]::Match($roughdates, '(^\w+) (\d+)-(\d+)').Groups.Value
+$month = $dates[1]
+@{
+	'Start' = [datetime]"$month $($dates[2]) $year"
+	'End' = [datetime]"$month $($dates[3]) $year"
+} 
